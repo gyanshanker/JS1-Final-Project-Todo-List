541d8f789f8ab269ad8fb791d0a633fe5dd421aa
Gyan <gyanusa123@gmail.com>
2018-02-17 00:41:31 -0800

    more phase 1

--- js/scripts.js
+++ js/scripts.js
@@ -11,31 +11,52 @@ function Task(x) {
     this.completed = false;
 }
 
-//function assToActiveTaskList creates and adds a new task into active tasks list and update 
+//function addToActiveTaskList creates and adds a new task into active tasks list and update 
 function addToActiveTaskList(x) {
-    var newTask = new Task(x);
-    taskListActive.push(newTask);
+    if (x !== "") {
+        var newTask = new Task(x);
+        taskListActive.push(newTask);
+    }
     //console.log(taskListActive);
 }
 
 var displayActiveDiv = document.querySelector('#activetasklist');
-
+//function updateActiveTaskDisplayupdats list of active tasks as dislayed on screen
 function updateActiveTaskDisplay() {
     //console.log(taskListActive);
     let oldUL = document.querySelector('#activetasklistUL');
-    console.log(oldUL);
+    //console.log(oldUL);
     oldUL.remove();
     let ul = document.createElement('ul');
     ul.className = "list-group";
     ul.id = "activetasklistUL";
     displayActiveDiv.appendChild(ul);
     for (i = 0; i < taskListActive.length; i++ ) {
+
         let li = document.createElement('li');
-        li.textContent = taskListActive[i].task;
+        //li.textContent = taskListActive[i].task;
         li.className = "list-group-item";
+        li.innerHTML = '<input type="checkbox" aria-label="..." name="checked"> ' + taskListActive[i].task;
+        //console.log(li);
         ul.appendChild(li);
     }
 }
+
+//     var checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+//     for(var i = 0; i < checkboxes.length; i++) {
+//         checkboxes[i].addEventListener('change', function(e){
+//             if (e.srcElement.checked === true) {
+//                 console.log("Task Checked for action");
+//             } 
+//             // console.log(e);
+//             // console.log(e.target.type);
+//             // console.log(e.srcElement.checked);
+
+//             //console.log(taskListActive[i].task);
+//         });
+//     }
+// }
 
 var newTaskEntry = document.querySelector('#newTaskEntry');
 
@@ -44,6 +65,49 @@ newTaskEntry.addEventListener('submit', function(e) {
     updateActiveTaskDisplay();
     e.preventDefault();
 })
+
+var editRequest = document.querySelector('#editbutton');
+
+editRequest.addEventListener('click', function(e) {
+    console.log('edit request');
+    console.log(e);
+    e.preventDefault();
+})
+
+var completeRequest = document.querySelector('#completebutton');
+
+completeRequest.addEventListener('click', function(e) {
+    console.log('complete request');
+    console.log(e);
+    e.preventDefault();
+})
+
+var deleteRequest = document.querySelector('#deletebutton');
+
+deleteRequest.addEventListener('click', function(e) {
+    console.log('delete request');
+    console.log(e);
+    e.preventDefault();
+})
+
+// get all the checkboxes on the page
+//var checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+// add a change event listener
+// for(var i = 0; i < checkboxes.length; i++) {
+//     checkboxes[i].addEventListener('change', function(){
+//         console.log('the checkbox changed');
+//     });
+// }
+
+
+// document.querySelector("#activetasklist").addEventListener('click', function(e) {
+//     if (e.target.type === 'checkbox') {
+//         console.log(e);
+
+
+// })
+
 
 
 
