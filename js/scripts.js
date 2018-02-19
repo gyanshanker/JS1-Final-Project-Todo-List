3d64aab2528a7b84561c5ca50f771265468f1354
Gyan <gyanusa123@gmail.com>
2018-02-18 23:40:26 -0800

    complete final functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,38 @@
 //GLOBAL Variables
-var taskListActive = []; //list of active tasks 
+var taskListActive = []; //global list of active tasks 
+var taskListCompleted = []; //global list of completed tasks
+var taskListDeleted = []; //global list of deleted tasks
 var newTaskEntry = document.querySelector('#newTaskEntry');
+var addRequest = document.querySelector('#addbutton');
 var editRequest = document.querySelector('#editbutton');
 var completeRequest = document.querySelector('#completebutton');
 var deleteRequest = document.querySelector('#deletebutton');
 var displayActiveDiv = document.querySelector('#activetasklist');
-var taskBeingCompleted;
-var taskBeingEdited;
-var taskBeingDeleted;
+var taskBeingEdited = null;
 
 //Task is constructor function for a task in the lists todoList
-function Task(x) {   
-    this.task = x;
+function Task(task) {   
+    this.task = task;
     this.dateCreated = Date.now(); 
     this.completed = false;
 }
 
 //function addToActiveTaskList creates and adds a new task to active-tasks-list or 
 //adds and edited tassk on list
-function addToActiveTaskList(x) {
-    if (x !== "") {
-        if (taskBeingEdited == null) {
-            let newTask = new Task(x);
+function addToActiveTaskList(task) {
+    if (task !== "") {
+        if (taskBeingEdited === null) {
+            let newTask = new Task(task);
             taskListActive.push(newTask);
         } else {
-            taskBeingEdited.task = x;
+            taskBeingEdited.task = task;
             taskListActive.push(taskBeingEdited);
+            taskBeingEdited = null;
         } 
+    } else {
+        alert('Enter task please!');
     }
+    return;
 }
 
 //function updateActiveTaskDisplay deletes current list and creates a new list on screen
@@ -35,29 +40,68 @@ function updateActiveTaskDisplay() {
     let oldUL = document.querySelector('#activetasklistUL');
     oldUL.remove();
     let ul = document.createElement('ul');
-    ul.className = "list-group";
     ul.id = "activetasklistUL";
+    var li;
+    //if there are no to dos in active or completed state, ..
+    if (taskListActive.length == 0 && taskListCompleted.length == 0) {
+        li = document.createElement('li');
+        li.textContent = "No ACTIVE To Dos";
+        ul.appendChild(li);
+        li = document.createElement('li');
+        li.textContent = "No COMPLETED To Dos";
+        ul.appendChild(li);
+        displayActiveDiv.appendChild(ul);
+        return;
+    }
+ 
+    ul.className = "list-group";
     displayActiveDiv.appendChild(ul);
+    let i = 0;
+    //add currently active tasks in list
     for (i = 0; i < taskListActive.length; i++ ) {
-        let li = document.createElement('li');
+        li = document.createElement('li');
         li.className = "list-group-item";   
-        if (taskListActive[i].completed) {
-            li.classList.add("line");
-            li.innerHTML = '<input type="checkbox" aria-label="..." name="mycheckedbox" disabled="true"> ' + taskListActive[i].task;
-        } else {
-            li.innerHTML = '<input type="checkbox" aria-label="..." name="mycheckedbox"> ' + taskListActive[i].task;
-        }
+        li.innerHTML = '<input type="checkbox" aria-label="..." name="mycheckedbox"> ' + taskListActive[i].task;
         ul.appendChild(li);
     }
+
+    //add completed tasks in list if any
+    for (i = 0; i < taskListCompleted.length; i++ ) {
+        li = document.createElement('li');
+        li.className = "list-group-item";   
+        li.classList.add("line");
+        li.innerHTML = '<input type="checkbox" aria-label="..." name="mycheckedbox" disabled="true"> ' + taskListCompleted[i].task;
+        ul.appendChild(li);
+    }
+    return;
 }
 
-//Event triggered when a new task is entered
+//Event triggered when a new task is entered clicking SUBMIT button
 newTaskEntry.addEventListener('submit', function(e) {
     addToActiveTaskList(newTaskEntry.newToDo.value); //create a new task and add to active task list
     newTaskEntry.newToDo.value = "";
     updateActiveTaskDisplay();
     e.preventDefault();
 })
+
+//Event triggered when user wants add a task clicking "Add a new ToDo" button
+addRequest.addEventListener('click', function(e) {
+    addToActiveTaskList(newTaskEntry.newToDo.value); //create a new task and add to active task list
+    newTaskEntry.newToDo.value = "";
+    updateActiveTaskDisplay();
+    e.preventDefault();
+})
+
+//Event  triggered when a user hits ENTER key to add a new event
+newTaskEntry.addEventListener('keydown', function (e) {
+    //check to see if the enter key was pressed
+    if (e.key === "Enter") {
+        addToActiveTaskList(newTaskEntry.newToDo.value); //create a new task and add to active task list
+        newTaskEntry.newToDo.value = "";
+        updateActiveTaskDisplay();
+        e.preventDefault();
+    }
+  });
 
 //Event triggered when user wants edit a task
 editRequest.addEventListener('click', function(e) {
@@ -68,10 +112,9 @@ editRequest.addEventListener('click', function(e) {
 //function to edit todo item
 function editTasks() {
     var checkedBoxes = document.querySelectorAll('input[name=mycheckedbox]');
-    taskBeingEdited = null;
 
     if (taskListActive.length === 0) {
-        alert('No active task');
+        alert('To Do List is empty');
         return;
     }
 
@@ -84,7 +127,7 @@ function editTasks() {
             return;
         }
     }
-    alert("No task checked to edit"); 
+    alert("No To Do checked for Edit"); 
 }
 
 //Event triggered when a user wants to mark a task completed
@@ -96,7 +139,7 @@ completeRequest.addEventListener('click', function(e) {
 
 function completeTask() {
     var checkedBoxes = document.querySelectorAll('input[name=mycheckedbox]');
-    taskBeingCompleted = null;
+    var taskBeingCompleted = null;
 
     if (taskListActive.length === 0) {
         alert('No active task');
@@ -108,12 +151,12 @@ function completeTask() {
             let temp = taskListActive.splice(i,1);
             taskBeingCompleted = temp[0];
             taskBeingCompleted.completed = true;
-            taskListActive.push(taskBeingCompleted);
+            taskListCompleted.push(taskBeingCompleted);
             updateActiveTaskDisplay();
             return;
         }
     }
-    alert('No task checked to mark completed');
+    alert('No To Do checked to mark completed');
 }
 
 //Event triggered whe a user wants to delete a task
@@ -126,7 +169,7 @@ deleteRequest.addEventListener('click', function(e) {
 
 function deleteTask() {
     var checkedBoxes = document.querySelectorAll('input[name=mycheckedbox]');
-    taskBeingDeleted = null;
+    var taskBeingDeleted = null;
 
     if (taskListActive.length === 0) {
         alert('No active task');
@@ -135,9 +178,10 @@ function deleteTask() {
 
     for (let i = 0; i < checkedBoxes.length; i++) {
         if (checkedBoxes[i].checked) {
-            if (confirm('Do you really want to delete the todo item?')) {
+            if (confirm('Do you really want to delete the To Do - ' + taskListActive[i].task + '?')) {
                 let temp = taskListActive.splice(i, 1);
                 taskBeingDeleted = temp[0];
+                taskListDeleted.push(taskBeingDeleted);
                 updateActiveTaskDisplay();
                 return;
             } else {
@@ -146,7 +190,7 @@ function deleteTask() {
 
         }
     }
-    alert('No task checked to delete');
+    alert('No To Do checked to delete');
 }
 
 
